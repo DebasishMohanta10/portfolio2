7089ef52ad5988a9a0eaccd5139a37b42e449c7f
DebasishMohanta <debasish2018rta@gmail.com>
2022-10-09 23:39:31 +0530

    portfolio2 website with JS effect

---
+++ scrollreveal.js
@@ -0,0 +1,41 @@
+ScrollReveal().reveal('header',{reset: true})
+ScrollReveal().reveal('.headline', { distance: "100px",origin: "left"});
+ScrollReveal().reveal('.tagline', { delay: 500,distance: "100px",origin: "left" });
+ScrollReveal().reveal('.web', { delay: 800,distance: "100px",origin: "left" });
+ScrollReveal().reveal('.punchline', { delay: 1000 });
+
+
+ScrollReveal().reveal('#about');
+ScrollReveal().reveal('.about-headline', { distance: "100px",origin: "left"});
+ScrollReveal().reveal('.about-tagline', { delay: 500,distance: "100px",origin: "left" });
+ScrollReveal().reveal('.about-text', { delay: 800,distance: "100px",origin: "left" });
+ScrollReveal().reveal('.about-btn', { delay: 1000,distance: "100px",origin: "left" });
+ScrollReveal().reveal('.stacked-cards', { delay: 800,distance: "100px",origin: "right" });
+
+
+ScrollReveal().reveal('#skills');
+ScrollReveal().reveal('.skills-headline', { distance: "100px",origin: "right"});
+ScrollReveal().reveal('.skills-tagline', { delay: 500,distance: "100px",origin: "right" });
+ScrollReveal().reveal('.skills-text', { delay: 800,distance: "100px",origin: "right" });
+ScrollReveal().reveal('.skills-progress', { delay: 1000,distance: "100px",origin: "bottom" });
+
+ScrollReveal().reveal('.skills-card', { delay: 1200,distance: "100px",origin: "bottom" });
+
+ScrollReveal().reveal('#portfolio');
+ScrollReveal().reveal('.portfolio-headline', { distance: "100px",origin: "left"});
+ScrollReveal().reveal('.portfolio-text', { delay: 500,distance: "100px",origin: "left"});
+
+ScrollReveal().reveal('.education-headline', { distance: "100px",origin: "right"});
+ScrollReveal().reveal('.education-tagline', { delay: 500,distance: "100px",origin: "right" });
+ScrollReveal().reveal('.education-text', { delay: 800,distance: "100px",origin: "right" });
+ScrollReveal().reveal('.timeline', { delay: 1000,distance: "100px",origin: "bottom" });
+
+ScrollReveal().reveal('.timeline__items', { delay: 1200,distance: "100px",origin: "right" });
+
+ScrollReveal().reveal('.achievement-1', { delay: 800,distance: "100px",origin: "left" });
+ScrollReveal().reveal('.achievement-2', { delay: 1300,distance: "100px",origin: "left" });
+
+ScrollReveal().reveal('.contact-headline', { distance: "100px",origin: "left"});
+ScrollReveal().reveal('.contact-tagline', { delay: 500,distance: "100px",origin: "left" });
+ScrollReveal().reveal('.contact-text', { delay: 800,distance: "100px",origin: "left" });
+ScrollReveal().reveal('.contact-list', { delay: 1000,distance: "100px",origin: "left" });
---
+++ typed.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typed', {
+    strings: ['I am a <b>Web Designer</b>', 'I am a <b>Web Developer</b>','I am a <b>Software Developer</b>'],
+    loop: true,
+    smartBackspace: true, // Default value
+    typeSpeed: 40,
+});
